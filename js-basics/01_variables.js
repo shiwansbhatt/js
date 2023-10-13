d494fc6aede7b885c79b56a4e94896f89e4a7547
shiwansbhatt <39997483+shiwansbhatt@users.noreply.github.com>
2023-10-13 07:15:54 +0000

    variables and datatypes

---
+++ js-basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1234567;
+let accountEmail = "shiwansbhatt99@gmail.com"
+var accountPassword = "qwertyuiop" //prefer not to use
+accountCity = "Dehradun"
+let accountState
+
+// accountId = 2
+console.log(accountPassword);
+console.table([accountId,accountEmail,accountPassword,accountState])
+
+
---
+++ js-basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";     //treat allJS code as newer version
+
+// alert("hello")  // suitable in browser not in node
+
+let name ="shiwans"
+let age = 24
+let isLoggedIn = false
+let state = null
+let user;
+
+//number,bigInt,string,boolean,null(standalone value||empty value),undefined(no value assigned),symbol(unique)----------primitive
+console.log(typeof user)
