5ca4696bfb40dc873749f86395ff8cca3fbe0419
shiwansbhatt <39997483+shiwansbhatt@users.noreply.github.com>
2023-10-13 09:07:37 +0000

    operation and conversion

---
+++ js-basics/03_typeConversion.js
@@ -0,0 +1,12 @@
+let score = 33
+let scoreInNumber = String(score)
+
+console.log(typeof(score))
+console.log(typeof(scoreInNumber))
+console.log(scoreInNumber);
+
+// 1-> true
+// 0-> false
+//""-> false
+//"shiwans"->true
+//to always check NaN 
---
+++ js-basics/04_operations.js
@@ -0,0 +1,20 @@
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2**2)
+// console.log(2**3)
+
+let str1="hello "
+let str2="user"
+// console.log(str1+str2)
+
+
+// console.log("1"+2)
+// console.log("1"+"2")
+// console.log(1+"2")
+// console.log(1+1+"3")
+
+let counter = 100
+console.log(++counter);
