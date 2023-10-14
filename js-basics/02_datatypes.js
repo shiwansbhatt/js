5e215cc8e991bb8008aa98a002619a9d92a88e00
shiwansbhatt <39997483+shiwansbhatt@users.noreply.github.com>
2023-10-14 02:35:05 +0000

    datatypes and strings

--- js-basics/02_datatypes.js
+++ js-basics/02_datatypes.js
@@ -9,4 +9,30 @@ let state = null
 let user;
 
 //number,bigInt,string,boolean,null(standalone value||empty value),undefined(no value assigned),symbol(unique)----------primitive
-console.log(typeof user)
+// console.log(typeof user)
+
+
+const id1 = Symbol('123')
+const id2 = Symbol('123')
+
+console.log(id1)
+console.log(id2)
+console.log(id1 === id2)
+
+
+
+//Arrays,objects,functions
+
+const cricketers = ["kohli","raina","yuvi"]
+
+let myDetails = {
+    name : "shiwans",
+    age : 24
+}
+
+const myDemoFunction = function(){
+    console.log("hello world")
+}
+myDemoFunction()
+
+
