fb6973f2803494a5b3e06d7fd959e7e206316109
shiwansbhatt <39997483+shiwansbhatt@users.noreply.github.com>
2023-10-14 06:57:18 +0000

    date and time

---
+++ js-basics/07_dateAndTime.js
@@ -0,0 +1,19 @@
+//dates
+
+
+let date = new Date();
+// console.log(date.toString())
+// console.log(date.toDateString())
+// console.log(date.toLocaleString())
+// console.log(typeof date)
+
+let myDate = new Date(2023,1,14,5 ,0)
+let myDate1 = new Date("2023-02-14")
+// console.log(myDate.toLocaleString());
+// console.log(myDate1.toLocaleString());
+
+let timeStamp = Date.now()
+console.log(timeStamp)
+
+
+
