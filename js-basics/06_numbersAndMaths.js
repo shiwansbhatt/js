44e6f2e7304001fe666dcdd81d390e68480a8780
shiwansbhatt <39997483+shiwansbhatt@users.noreply.github.com>
2023-10-14 05:00:25 +0000

    string,maths And Numbers

---
+++ js-basics/06_numbersAndMaths.js
@@ -0,0 +1,30 @@
+const score = 400
+//console.log(score)
+
+const balance = new Number(300) 
+//console.log(balance);
+
+//console.log(balance.toString());
+
+//console.log(balance.toFixed(1));
+
+const floatNumber = 489.82345
+// console.log(floatNumber.toPrecision(5));
+
+//////////////////////////////
+
+
+console.log(Math.abs(-4))
+
+console.log(Math.round(4.8))
+
+console.log(Math.sqrt(25))
+
+console.log(Math.floor((Math.random()*10) + 1))
+
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+
